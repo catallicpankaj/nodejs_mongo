514533298c9b7a7c359b407b4377b5fcd38cd6ac
catallicpankaj <catallic.pankaj@gmail.com>
2019-02-15 14:07:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 let express = require('express');
+let cors = require('cors');
 let mongoose = require('mongoose').set("debug",true);
 
 let bodyParser = require('body-parser');
 
-let port= process.env.PORT||1010;
+let port= process.env.PORT||2020;
 let app=express();
 
 
 let apiRoutes = require("./api/api-routes/api-routing.js");
 var url = "mongodb://test-mongo-db:1PDx6SLX72MxCgN4CO6HS6WbpiKXjs4kj7hkXVMYP6oTHvSIzmJLsWxw6BGn83cRkU6LsxdiVf94Tq67Q4FAHA%3D%3D@test-mongo-db.documents.azure.com:10255/esg_db?ssl=true";
 
+app.use(cors());
 //bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
     extended: true
